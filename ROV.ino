9f1a2ff017cdbd6646a3a16344c88778290f25e3
Felix Wong <fk2wong@uwaterloo.ca>
2017-10-20 05:35:24 -0400

    Code for checkpoint2, runs front motors based on pitch&roll.

--- ROV.ino
+++ ROV.ino
@@ -1,3 +1,4 @@
+#include <Servo.h>
 #include "attitude.h"
 
 /************************************************************
@@ -15,15 +16,82 @@ Supported Platforms:
 - ATSAMD21 (Arduino Zero, SparkFun SAMD21 Breakouts)
 *************************************************************/
 
+// checkpoint2 stuff
+
+int PORTBR = 13;
+int PORTBL = 12;
+int PORTBC = 11;
+int PORTFR = 9;
+int PORTFL = 10;
+
+Servo motorBL;
+Servo motorBR;
+Servo motorBC;
+
+Servo motorFL;
+Servo motorFR;
+
+#define MTR_HIGH 180
+#define STOP 90
+#define MTR_LOW 0
+
+#define MTR_CONSTRAIN(x) (max(min((x), MTR_HIGH), MTR_LOW))
+
+void initMotors()
+{
+  motorBR.attach(PORTBR);
+  motorBL.attach(PORTBL);
+  motorBC.attach(PORTBC);
+  motorFR.attach(PORTFR);
+  motorFL.attach(PORTFL);
+
+  delay(1000);
+  
+  motorBR.write(STOP);
+  motorBL.write(STOP);
+  motorBC.write(STOP);
+  motorFR.write(STOP);
+  motorFL.write(STOP);
+  
+  Serial.println("motors initialized!");
+}
+
+void outputMotors(double pitch, double roll, double yaw)
+{
+  int BRval, BLval, BCval, FRval, FLval;
+  int gain = 3;
+
+  FRval = MTR_CONSTRAIN(STOP+gain*(pitch/2+roll/2));
+  FLval = MTR_CONSTRAIN(STOP+gain*(pitch/2-roll/2));
+  BRval = MTR_CONSTRAIN(STOP);
+  BLval = MTR_CONSTRAIN(STOP);
+  BCval = MTR_CONSTRAIN(STOP-gain*pitch);
+
+  Serial.print(FRval);
+  Serial.print("\t");
+  Serial.print(FLval);
+  Serial.print("\t");
+  Serial.println(BCval);
+  
+  motorFR.write(FRval);
+  motorFL.write(FLval);
+  motorBL.write(BLval);
+  motorBR.write(BRval);
+  motorBC.write(BCval);
+}
+
+//
+
 Attitude attitude;
 
 void setup() 
 {
   Wire.begin();
   Serial.begin(38400);
+  
   attitude.init();
 
-  delay(50);
+  initMotors();
 }
 
 void loop() 
@@ -32,7 +100,9 @@ void loop()
   
   attitude.getUpdatedAxes(&pitch, &roll, &yaw);
 
-  printAxes(pitch, roll, yaw);
+  //printAxes(pitch, roll, yaw);
+
+  outputMotors(pitch, roll, yaw);
 
   delay(10);
 }
@@ -51,4 +121,3 @@ void printAxes(double pitch, double roll, double yaw)
   Serial.print(yaw); 
   Serial.print("\r\n");
 }
-
