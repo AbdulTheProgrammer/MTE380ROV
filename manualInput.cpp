d761008019b44f49d5a56f921ed0276fd6438aae
Felix Wong <fk2wong@uwaterloo.ca>
2017-11-16 03:23:10 -0500

    Rebased with manualInput library, fixed errors.

--- manualInput.cpp
+++ manualInput.cpp
@@ -5,14 +5,9 @@
 #define LX_PIN 1
 #define LY_PIN 2
 #define LD_PIN 3
-
 #define RX_PIN 4
 #define RY_PIN 5
 #define RD_PIN 6
-
-#define MAX_SPEED 180
-#define NEUTRAL_SPEED 90
-#define MIN_SPEED 0
 
 #define MIN_UPDATE_TIME_US 100*1000
 #define MIN_UPDATE_FREQ_HZ (1000000/MIN_UPDATE_TIME_US)
@@ -21,37 +16,35 @@
 #define MAX_ROTATION_PER_UPDATE (MAX_ROTATION_SPEED_DEG_PER_SEC / MIN_UPDATE_FREQ_HZ)
 
 
-ManualInput::ManualInput(double &setPointYaw, double &setPointThrust, double &setPointDepth)
+ManualInput::ManualInput(void) :
+  _Joystick(LX_PIN, LY_PIN, LD_PIN, RX_PIN, RY_PIN, RD_PIN)
 {
-  inputYaw = setPointYaw;
-  inputDepth = setPointDepth;
-  inputThrust = setPointThrust;
-  Joystick(LX_PIN, LY_PIN, LD_PIN, RX_PIN, RY_PIN, RD_PIN);
-  lastUpdate = 0;
+  _lastUpdate = 0;
 }
 
-void ManualInput::addJoystickInput(void)
+void ManualInput::GetJoystickInput(double &setPointYawChange, double &setPointThrust, double &setPointDepth)
 {
-  int lx = Joystick.readLX();
-  int ly = Joystick.readLY();
-  int rx = Joystick.readRX();
-  int ry = Joystick.readRY();
+  int lx = _Joystick.readLX();
+  int ly = _Joystick.readLY();
+  int rx = _Joystick.readRX();
+  int ry = _Joystick.readRY();
 
   //set motor values based on -100 to 100 scale of values
   //THURST
-  *inputThrust = ly;
+  setPointThrust = ly;
 
   //YAW
   unsigned long Now = micros();
-  unsigned long timeDiff = Now - lastUpdate;
+  unsigned long timeDiff = Now - _lastUpdate;
   if (timeDiff > MIN_UPDATE_TIME_US) // if its been longer than 10ms
   {
-    //Increment yaw by some reasonable
-    *inputYaw += (rx/100.0) * MAX_ROTATION_PER_UPDATE;
-    lastUpdate = Now;
+    // TODO read from controls to get the current yaw
+    
+    //Increment yaw by some reasonable amount
+    setPointYawChange = (rx/100.0) * MAX_ROTATION_PER_UPDATE;
+    _lastUpdate = Now;
   }
 
   //DEPTH
-  *inputDepth = map(ry, -100, 100, 0, 100);
-
+  setPointDepth = map(ry, -100, 100, 0, 100);
 }
